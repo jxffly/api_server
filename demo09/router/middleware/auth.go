953deff1376a6d68fb9e6c5e438ebd81007357f6
xiaofeijin <xiaofeijin@tencent,com>
2020-02-02 18:43:27 +0800

    fixed bugs

--- demo09/router/middleware/auth.go
+++ demo09/router/middleware/auth.go
@@ -6,12 +6,14 @@ import (
 	"apiserver_demos/demo09/pkg/token"
 
 	"github.com/gin-gonic/gin"
+	"github.com/lexkong/log"
 )
 
 func AuthMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		// Parse the json web token.
 		if _, err := token.ParseRequest(c); err != nil {
+			log.Error("parse token occur err", err)
 			handler.SendResponse(c, errno.ErrTokenInvalid, nil)
 			c.Abort()
 			return
